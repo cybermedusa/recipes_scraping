873e743dda066632e261a07921c9bbc46c30c50d
zuzka98 <zuzannakilar@gmail.com>
2022-04-08 12:41:57 +0200

    SOW - sub_obj in range(len()) loop - new scenario

--- sow/sow_scrape_preparation.py
+++ sow/sow_scrape_preparation.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from requests import get
 from sow_scrape_titles import links_list
 import json
-from sow_merging import h4_links, p_links, ul_links, p_ul_links
+from sow_merging import h4_links, p_links, ul_links, p_ul_links, irregular_h4
 
 h4 = []
 p = []
@@ -45,22 +45,23 @@ def get_preparation(arr):
 
 recipe_h4 = []
 recipes_h4 = []
+
 for link in h4_links:
     new_page = get(link)
     b_s = BeautifulSoup(new_page.content, 'html.parser')
     full_content = list(filter(lambda x: x != '\n', b_s.select_one('.method-body').contents))
     clean_content = list(filter(lambda x: x.name == 'h4' or x.name == 'ul', full_content))
-    sub_obj = {
-        "title": '',
-        "content": ''
-    }
+    for i in range(0, len(clean_content), 2):
+        title = clean_content[i]
+        content = clean_content[i + 1]
 
-    for i in range(len(clean_content)):
-
-        if clean_content[i].name == 'h4':
-            title = clean_content[i].get_text().strip()
-            sub_obj["title"] = title
-
-        if clean_content[i].name == 'ul':
-            content = [clean_content[i].get_text().strip()]
-            sub_obj["content"] = get_preparation(content)
+        sub_obj = {
+            "title": '',
+            "content": ''
+        }
+        title = title.get_text().strip()
+        sub_obj['title'] = title
+        content = [content.get_text().strip()]
+        sub_obj['content'] = get_preparation(content)
+        recipe_h4.append(sub_obj)
+    recipes_h4.append(recipe_h4)
