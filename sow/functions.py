b275e5eca820a7a1a53e3e36abd9d47cad849986
zuzka98 <zuzannakilar@gmail.com>
2022-04-08 14:44:48 +0200

    SOW - create functions.py module to store all the functions

---
+++ sow/functions.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+h4 = []
+p = []
+ul = []
+p_ul = []
+
+
+def fit_scenario(link_arr):
+    global h4, ul, p, p_ul
+
+    for link in link_arr:
+        new_page = get(link)
+        b_s = BeautifulSoup(new_page.content, 'html.parser')
+        c = list(filter(lambda x: x != '\n', b_s.select_one('.method-body').contents))
+        if c[0].name == 'h4':
+            h4.append(link)
+
+        if c[0].name == 'ul':
+            ul.append(link)
+
+        if c[0].name == 'p' and c[1].name != 'ul':
+            p.append(link)
+
+        if c[0].name == 'p' and c[1].name == 'ul':
+            p_ul.append(link)
+
+    return h4, ul, p, p_ul
+
+
+def get_value(arr):
+    val_arr = []
+    for val in arr:
+        val_arr.append({
+            "text": val
+        })
+    return val_arr
