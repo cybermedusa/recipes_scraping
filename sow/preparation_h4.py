f49d76db24acf7670fdd37372b917cd76d699bad
zuzka98 <zuzannakilar@gmail.com>
2022-04-08 14:44:20 +0200

    SOW - move functions to functions.py module, overall cleaning

---
+++ sow/preparation_h4.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+from requests import get
+import json
+from links_array import h4_links
+from functions import get_value
+
+recipe_h4 = []
+recipes_h4 = []
+dishes_preparation = []
+for link in h4_links:
+    new_page = get(link)
+    b_s = BeautifulSoup(new_page.content, 'html.parser')
+    full_content = list(filter(lambda x: x != '\n', b_s.select_one('.method-body').contents))
+    clean_content = list(filter(lambda x: x.name == 'h4' or x.name == 'ul', full_content))
+    for i in range(0, len(clean_content), 2):
+        title = clean_content[i]
+        content = clean_content[i + 1]
+
+        sub_obj = {
+            "title": '',
+            "content": ''
+        }
+        title = title.get_text().strip()
+        sub_obj['title'] = title
+        content = [content.get_text().strip()]
+        sub_obj['content'] = get_value(content)
+        recipe_h4.append(sub_obj)
+    recipes_h4.append(recipe_h4)
+    dish = {
+        "name": link,
+        "recipe": recipes_h4
+    }
+    dishes_preparation.append(dish)
+    recipe_h4 = []
+    recipes_h4 = []
+
+    json_object = json.dumps(dishes_preparation, indent=4)
+
+    with open("recipes_h4.json", "w") as outfile:
+        outfile.write(json_object)
