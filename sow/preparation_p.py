567227210f8e05d077a1697db22b614780c6942a
zuzka98 <zuzannakilar@gmail.com>
2022-04-12 14:29:35 +0200

    SOW - merged all recipes with p and h4 as main characters

--- sow/preparation_p.py
+++ sow/preparation_p.py
@@ -3,7 +3,7 @@ import itertools as it
 from requests import get
 import json
 from links_array import p_with_h4
-from functions import get_value, return_arr
+from functions import get_value
 
 recipe_p = []
 recipes_p = []
@@ -12,12 +12,11 @@ for link in p_with_h4:
     new_page = get(link)
     b_s = BeautifulSoup(new_page.content, 'html.parser')
     full_content = list(filter(lambda x: x != '\n', b_s.select_one('.method-body').contents))
-    # [idx for idx, val in enumerate(full_content) if val.name == "div"].pop(0)
-    # [idx for idx, val in enumerate(full_content) if val.name == "h4"].pop(0)
-    if full_content[0].name != 'h4':
-        full_content = full_content[1:]
 
-    full_content = full_content[:-3]
+    first_idx = [idx for idx, val in enumerate(full_content) if val.name == "h4"].pop(0)
+    last_idx = [idx for idx, val in enumerate(full_content) if val.name == "div"].pop(0)
+    full_content = full_content[first_idx:last_idx]
+
     titles = []
     for i in full_content:
         if i.name == "h4":
@@ -50,12 +49,5 @@ for link in p_with_h4:
 
     json_object = json.dumps(dishes_preparation, indent=4)
 
-    with open("recipes_p_with_h4.json", "w") as outfile:
+    with open("recipes_p.json", "w") as outfile:
         outfile.write(json_object)
-
-
-
-
-
-
-
