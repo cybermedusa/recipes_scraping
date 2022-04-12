06d68cb4ea8eeb8f06d3c3cb4c628308f5d17125
zuzka98 <zuzannakilar@gmail.com>
2022-04-12 14:31:04 +0200

    SOW - scraped all recipes with ul and h4 as main characters

---
+++ sow/preparation_ul.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+from requests import get
+import json
+from links_array import ul_links
+from functions import get_value
+
+recipe_ul = []
+recipes_ul = []
+dishes_preparation = []
+for link in ul_links:
+    new_page = get(link)
+    b_s = BeautifulSoup(new_page.content, 'html.parser')
+    full_content = list(filter(lambda x: x != '\n', b_s.select_one('.method-body').contents))
+    clean_content = list(filter(lambda x: x.name == 'h4' or x.name == 'ul', full_content))
+    clean_content.insert(0, "Preparation")
+    for i in range(0, len(clean_content), 2):
+        title = clean_content[i]
+        content = clean_content[i + 1]
+
+        sub_obj = {
+            "title": '',
+            "content": ''
+        }
+        title = title if clean_content[i] == 'Preparation' else title.get_text()
+        sub_obj['title'] = title
+        content = [content.get_text().strip()]
+        sub_obj['content'] = get_value(content)
+        recipe_ul.append(sub_obj)
+    recipes_ul.append(recipe_ul)
+    dish = {
+        "name": link,
+        "recipe": recipes_ul
+    }
+    dishes_preparation.append(dish)
+    recipe_ul = []
+    recipes_ul = []
+
+    json_object = json.dumps(dishes_preparation, indent=4)
+
+    with open("recipes_ul.json", "w") as outfile:
+        outfile.write(json_object)
